679fbcf7c96339335a1a761770880e353a00d55f
Kovács Hanna <kovacs.hanna.bianka@gmail.com>
2021-11-12 10:12:13 +0100

    labda es labdagyar

---
+++ santafactory/santafactory/Entities/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace santafactory.Entities
+{
+    public class Ball : Label 
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        private void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+
+    }
+}
