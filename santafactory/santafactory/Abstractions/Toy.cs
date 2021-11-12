540d890421f7482a643de770ede36d4bfa2c0e66
Kovács Hanna <kovacs.hanna.bianka@gmail.com>
2021-11-12 10:37:49 +0100

    Játék absztrakció kezelése

---
+++ santafactory/santafactory/Abstractions/Toy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace santafactory.Abstractions
+{
+    public class Toy : Label 
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        private void DrawImage(Graphics g)
+        {
+            //throw new NotImplementedException();
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveToy()
+        {
+            Left += 1;
+        }
+
+    }
+}
