9e6da0480ba65ac58c41341be9e4782469c88604
Kovács Hanna <kovacs.hanna.bianka@gmail.com>
2021-11-12 10:55:00 +0100

    Autó létrehozása

--- santafactory/santafactory/Entities/Car.cs
+++ santafactory/santafactory/Entities/Car.cs
@@ -13,6 +13,10 @@ namespace santafactory.Entities
         protected override void DrawImage(Graphics g)
         {
             throw new NotImplementedException();
+
+            var image = Image.FromFile(@"Images\car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+
         }
     }
 }
