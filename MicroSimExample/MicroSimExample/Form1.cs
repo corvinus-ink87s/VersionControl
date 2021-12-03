b46eb0ed9ca5423d96917b63afbab799669f8181
Kovács Hanna <kovacs.hanna.bianka@gmail.com>
2021-12-03 10:25:00 +0100

    adatok beolvasása

--- MicroSimExample/MicroSimExample/Form1.cs
+++ MicroSimExample/MicroSimExample/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MicroSimExample.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,44 @@ namespace MicroSimExample
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
+
+
         public Form1()
         {
             InitializeComponent();
+
+            Population = Getpopulation(@"C:\Temp\nép-teszt.csv");
+        }
+
+        public List<Person> Getpopulation(string csvPath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvPath, Encoding.Default))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                   var line = sr.ReadLine().Split(';');
+                    var p = new Person();
+                    p.BirthYear = int.Parse(line[0]);
+                    p.Gender = (Gender)Enum.Parse(typeof(Gender), line[1]);
+                    p.NbrOfChildren = int.Parse(line[2]);
+                    population.Add(p);
+                }
+            }
+            
+            return population;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
